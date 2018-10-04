fbfbfefc29cf5e093f961893950374d2ce210949
Nigel Packer <packern@wwu.edu>
2018-10-04 11:28:27 -0700

    Implement our own tree builder

--- src/token-browser.js
+++ src/token-browser.js
@@ -1,1 +1,8 @@
-(function () {})();
+(function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.tokenBrowser = {
+    attach: function (context, settings) {
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
