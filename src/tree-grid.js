be71da86c0963c9724a2e8c684789811a2be7ffe
Nigel Packer <packern@wwu.edu>
2018-10-04 17:10:31 -0700

    Add data attribute to table

--- src/tree-grid.js
+++ src/tree-grid.js
@@ -1,8 +1,27 @@
-(function ($, Drupal, window, document, undefined) {
+(function ($, Drupal, window, ducment, undefined) {
+
+  'use strict';
+
+  var types = {};
+
+  function click_button(event) {
+  }
 
   Drupal.behaviors.treeGrid = {
     attach: function (context, settings) {
+      var $jsonXHR = $.getJSON(settings.basePath + '/token/browser/types');
+      var $tg = $('.tree-grid', context);
+      var $button_cells = $tg.find('td:first-child');
+      var $button = $('<button>Expand</button>');
+
+      $jsonXHR.done(function (data) {
+        types = data;
+        $button_cells.prepend($button);
+      });
+
+      $button.on('click', click_button);
     }
   };
+
 
 })(jQuery, Drupal, this, this.document);
