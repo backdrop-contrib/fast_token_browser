ac29ba1bc31cef707fdbc1433f0798da5fe70878
Nigel Packer <packern@wwu.edu>
2018-10-09 11:33:51 -0700

    Add initial behavior to render overlay

--- src/token-browser.js
+++ src/token-browser.js
@@ -1,2 +1,35 @@
 (function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.tokenBrowserDialog = {
+    attach: function (context, settings) {
+      $('a.token-browser', context).once('token-dialog').click(function (event) {
+        var $this = $(this);
+        var dialog = $('<div>').addClass('loading').css({ display: 'none' }).appendTo('body');
+        var url = $this.attr('href');
+        var data = {};
+
+        data['ajax_page_state[theme]'] = settings.ajaxPageState.theme;
+        data['ajax_page_state[theme_token]'] = settings.ajaxPageState.theme_token;
+
+        dialog.dialog({
+          title: Drupal.t('Token Browser'),
+          width: 700,
+          close: function (event, ui) {
+            dialog.remove();
+          }
+        });
+
+        dialog.load(
+          url,
+          data,
+          function (responseText, textStatus, XMLHttpRequest) {
+            dialog.removeClass('loading');
+          }
+        );
+
+        event.preventDefault();
+      });
+    }
+  };
+
 })(jQuery, Drupal, this, this.document);
